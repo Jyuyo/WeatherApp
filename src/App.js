243502eeef050dc597a6b6b938371620894b5801
Jyuyo <43424853+Jyuyo@users.noreply.github.com>
2020-06-11 20:33:06 -0500

    Responsive Design with React flexbox

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import LocationList from './Components/WeatherLocation/LocationList'
-import { render } from '@testing-library/react';
+import {Grid,Col,Raw, Row} from 'react-flexbox-grid';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 const cities = [
   'Zapopan,mx',
@@ -30,16 +34,43 @@ class App extends Component {
   render() {
     const {city} = this.state
       return(
-      <div className="App">
+        <Grid>
+          <Row>
+            <AppBar position='sticky'>
+              <ToolBar>
+                <Typography variant='title' color='inherit'>
+                  WeatherApp
+                </Typography>
+              </ToolBar>
+            </AppBar>
+          </Row>
+          <Row>
+            <Col xs={12} md={6}>
+              <LocationList cities={cities} onSelectedLocation={this.handleLocationSelect}/>
+            </Col>
+            <Col xs={12} md={6}>
+              <Paper elevation={4}>
+                <div className='details'>
+                {city ?
+                  <span>{city}</span> :
+                  <h5>No hay</h5>
+                }
+                </div>
+              </Paper>
+            </Col>
+          </Row>
+        </Grid>
+      );
+  }
+}
+
+/*<div className="App">
         <LocationList cities={cities} onSelectedLocation={this.handleLocationSelect}/>
 
       {city ?
       <span>{city}</span> :
       <h5>No hay</h5>
       }
-      </div>
-      );
-  }
-}
+      </div>*/ 
 
 export default App;
